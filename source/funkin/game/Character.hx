fc32a2380b36cd8fa4ae1cf1b3679e1b5d1757a8
drfiffi <90726534+drfiffi@users.noreply.github.com>
2026-07-27 18:55:07 -0400

    saves "centercam" to xml (#1035)
    * Fix Girlfriend's camera offsets (#973)
    
    * saves "centercam" to xml
    
    someone forgot to add this, so here ya go!
    
    ---------
    
    Co-authored-by: ADA_Funni <99497910+ADA-Funni@users.noreply.github.com>

--- source/funkin/game/Character.hx
+++ source/funkin/game/Character.hx
@@ -463,6 +463,7 @@ class Character extends FunkinSprite implements IBeatReceiver implements IOffset
 
 		if (cameraOffset.x != 0) xml.set("camx", Std.string(FlxMath.roundDecimal(cameraOffset.x, 2)));
 		if (cameraOffset.y != 0) xml.set("camy", Std.string(FlxMath.roundDecimal(cameraOffset.y, 2)));
+		if (centeredCamera != !isAnimate) xml.set("centercam", centeredCamera);
 
 		if (holdTime != 4) xml.set("holdTime", Std.string(FlxMath.roundDecimal(holdTime, 4)));
 
